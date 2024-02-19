1154d21b56c1a30fdb2b407bed74d802d2fd098f
mmeacham92 <meacham.michael@rocketmail.com>
2024-02-19 11:35:55 -0600

    created components dir along with ColorContainer and SearchColor components

---
+++ src/components/ColorContainer.tsx
@@ -0,0 +1,13 @@
+
+
+const ColorContainer: React.FC = () => {
+
+    return (
+        <div id="color__container">
+
+        </div>
+    );
+}
+
+
+export default ColorContainer;
---
+++ src/components/SearchColor.tsx
@@ -0,0 +1,18 @@
+
+
+const SearchColor: React.FC = () => {
+    
+    return (
+        <form action="">
+            <label htmlFor="search">Search Color</label>
+            <input
+                type="text"
+                id="search"
+                placeholder="Search Color"
+                role="searchbox"
+            />
+        </form>
+    );
+}
+
+export default SearchColor;
